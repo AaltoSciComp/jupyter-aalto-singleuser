862867ef4a2ead236ed2a638c052669962854a93
Sami Laine <sami.v.laine@aalto.fi>
2023-03-30 19:25:49 +0300

    standard: Clean both environments
    - Due to an oversight, the clean-layer.sh script hasn't actually been
      cleaning the /opt/software directory at all. As a result, temporary
      files have been included in the layers and file permissions have been
      incorrect for newly updated files.

--- scripts/clean-layer.sh
+++ scripts/clean-layer.sh
@@ -3,16 +3,19 @@ rm -rf /tmp/*
 
 rm -rf /home/$NB_USER/.cache/yarn
 
-conda clean --all --yes
-mamba clean --all --yes
+/opt/conda/bin/mamba clean --all --yes
+/opt/software/bin/mamba clean --all --yes
 mountpoint -q /opt/conda/pkgs/cache/ || rm -rf /opt/conda/pkgs/cache/
+mountpoint -q /opt/software/pkgs/cache/ || rm -rf /opt/software/pkgs/cache/
 mountpoint -q /root/.cache/pip/ || rm -rf /root/.cache/pip/*
+
 npm cache clean --force
+rm -rf /home/$NB_USER/.npm/_logs
 
 apt-get autoremove -y
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 
-fix-permissions $CONDA_DIR /home/$NB_USER
+fix-permissions /opt/conda /opt/software /home/$NB_USER
 
 } 2>&1 > /dev/null
