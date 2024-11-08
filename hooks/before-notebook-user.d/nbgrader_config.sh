fdafba6cd655034a4f5c813934d13e694d7b728d
Sami Laine <sami.v.laine@aalto.fi>
2024-11-08 17:23:49 +0200

    hooks: Add missing newline to nbgrader config on append

--- hooks/before-notebook-user.d/nbgrader_config.sh
+++ hooks/before-notebook-user.d/nbgrader_config.sh
@@ -1,6 +1,7 @@
-if [ -r /course/etc.jupyter.nbgrader_config.py.append -a -w /etc/jupyter/nbgrader_config.py ] ; then
+if [ -r /course/etc.jupyter.nbgrader_config.py.append ] && [ -w /etc/jupyter/nbgrader_config.py ] ; then
     echo >> /etc/jupyter/nbgrader_config.py
     echo "# Appended from /course/etc.jupyter.nbgrader_config.py.append" >> /etc/jupyter/nbgrader_config.py
     cat /course/etc.jupyter.nbgrader_config.py.append >> /etc/jupyter/nbgrader_config.py
+    echo >> /etc/jupyter/nbgrader_config.py
     echo "# end appended data" >> /etc/jupyter/nbgrader_config.py
 fi
