ccf5d6c3cbc38f5c26041af94ce3307ae54a08b7
Sami Laine <sami.v.laine@aalto.fi>
2025-01-03 14:20:51 +0200

    hooks: Add /course/pymod to PYTHONPATH

--- hooks/before-notebook-user.d/paths.sh
+++ hooks/before-notebook-user.d/paths.sh
@@ -7,6 +7,7 @@ IF_EXIST () { if [ -e "$1" ] ; then eval "${@:2}" ; fi } #eval 2 on if $1 exist
 PATHADD2 () { if eval [ x"\"\$$1\"" = x'' ] ; then eval "export $1=$2" ; else eval "echo \"\$$1\"" | grep "\(^\|:\)$2\(:\|$\)" 2>&1 > /dev/null || eval "export $1=\$$1:$2" ; fi }
 
 IF_EXIST /m/jhnas/jupyter/software/bin/    PATHADD2 PATH /m/jhnas/jupyter/software/bin/
+IF_EXIST /course/pymod/                    PATHADD2 PYTHONPATH /course/pymod/
 IF_EXIST /m/jhnas/jupyter/software/pymod/  PATHADD2 PYTHONPATH /m/jhnas/jupyter/software/pymod/
 
 unset IF_EXIST PATHADD2
