67ca79f5b680c38d95b47b2ab88cfa138e956021
Sami Laine <sami.v.laine@aalto.fi>
2025-12-10 19:17:59 +0200

    Add tests for julia imports

---
+++ tests/julia/test_linopt_imports.jl
@@ -0,0 +1,15 @@
+# linopt2026, RT#30747
+using Combinatorics
+using DelimitedFiles
+using HiGHS
+using JLD2
+using JuMP
+using LaTeXStrings
+using LinearAlgebra
+using PlotlyJS
+using Plots
+using Polyhedra
+using Random
+using Statistics
+using Test
+using JuMPModelPlotting
